997fc47ce05a5298599932bcda2ee94b2ee786a7
Glitch (clean-global-oboe) <none>
2020-06-19 03:20:26 +0000

    🐄🐔 Checkpoint ./Invest.html:17222529/112 ./script.js:17222529/528

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log('hi');
-
-// Instruction from https://materializecss.com/auto-init.html to initialize
-M.AutoInit();
-
-// Instruction from https://materializecss.com/navbar.html
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
-});
+alert("Disclaimer: This is not investment advisory" );
